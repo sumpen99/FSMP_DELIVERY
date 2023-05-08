1b9f127482715d5b17476c1744e138aa5ed25c2a
sumpen99 <sumpen841001@hotmail.com>
2023-05-08 17:55:02 +0200

    Initial Commit

---
+++ FSMP_DELIVERY/FSMP_DELIVERYApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSMP_DELIVERYApp.swift
+//  FSMP_DELIVERY
+//
+//  Created by fredrik sundström on 2023-05-08.
+//
+
+import SwiftUI
+
+@main
+struct FSMP_DELIVERYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
